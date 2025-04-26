8144b1ada909e4ecbdc9cc8bf2da77e8c5e3a048
Igor <webdevbiv@gmail.com>
2025-04-26 16:36:02 +0300

    UPD | NotFoundPage styles

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,9 +4,13 @@ const NotFoundPage = () => {
   const navigate = useNavigate();
   return (
     <div className={s.container}>
-      <h1>404 Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <p>Click the button below to go back to the homepage.</p>
+      <h1 className={s.title}>404 Not Found</h1>
+      <p className={s.text}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <p className={s.text}>
+        Click the button below to go back to the homepage.
+      </p>
       <button className={s.button} onClick={() => navigate('/')}>
         Go to Homepage
       </button>
